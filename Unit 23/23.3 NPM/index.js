665083704f3179c972bd738c39cb5f1d4fe3ac98
JNSStudios <joshuanschiavi@gmail.com>
2025-05-14 13:49:47 -0400

    Completed Unit 23 Lesson 3

---
+++ Unit 23/23.3 NPM/index.js
@@ -0,0 +1,13 @@
+// var sillyname = require('sillyname');
+
+import generateStupidName from "sillyname";
+
+var name = generateStupidName();
+
+console.log(`My silly name is ${name}.`);
+
+
+import { randomSuperhero } from "superheroes";
+name = randomSuperhero();
+
+console.log(`Fear not, for ${name} is here!! (cue the theme music)`);
