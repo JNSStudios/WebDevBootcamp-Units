0c7fc362ef40c22df94cf20a477eca5420bb60bb
JNSStudios <joshuanschiavi@gmail.com>
2025-05-15 11:59:00 -0400

    Completed and started more lessons

--- Unit 24/24.4 and 24.5 Middleware/index4.js
+++ Unit 24/24.4 and 24.5 Middleware/index4.js
@@ -4,10 +4,20 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+
 const app = express();
 const port = 3000;
+var name = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function infoGetter(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+  name = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(infoGetter);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
@@ -15,11 +25,12 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
   console.log(req.body);
-
+  res.send(`<h1>Your band name is</h1><h2>${name}</h2><p>...maybe don't quit your day job.</p>`);
 
 });
+
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
