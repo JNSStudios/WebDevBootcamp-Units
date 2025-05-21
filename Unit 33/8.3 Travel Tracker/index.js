c868e7a4be250f911a51731a650fda8cbfc4e45c
JNSStudios <joshuanschiavi@gmail.com>
2025-05-21 13:09:32 -0400

    Unit 33 Lesson 8 completed

--- Unit 33/8.3 Travel Tracker/index.js
+++ Unit 33/8.3 Travel Tracker/index.js
@@ -21,7 +21,6 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
 
   const result = await db.query("SELECT country_code FROM visited_countries");
-  db.end();
 
   let countries = [];
   result.rows.forEach((country) => {
@@ -32,6 +31,29 @@ app.get("/", async (req, res) => {
 
   res.render("index.ejs", { countries: countries, total: countries.length });
 });
+
+// handle adding new countries via form submission
+app.post("/add", async (req, res) => {
+  const country = req.body.country;
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [country]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query(
+      "INSERT INTO visited_countries (country_code) VALUES ($1)",
+      [countryCode]
+    );
+  }
+
+  console.log(result);
+  res.redirect("/");
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
